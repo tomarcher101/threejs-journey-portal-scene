7e61a98411d6c964804de57d1daa535619806766
Tom Archer <tomarcher@Toms-MacBook-Pro-16.local>
2024-01-07 22:50:00 +1100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import glsl from 'vite-plugin-glsl'
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: '/<REPO>',
+    base: '/threejs-journey-portal-scene/',
     server:
     {
         host: true, // Open to local network and display URL
