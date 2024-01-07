193b9e09d9afb3d0d4898e59b194c75a37ab4b4c
Tom Archer <tomarcher@Toms-MacBook-Pro-16.local>
2024-01-07 22:47:37 +1100

    remove build files and create deployment action

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import glsl from 'vite-plugin-glsl'
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/<REPO>',
     server:
     {
         host: true, // Open to local network and display URL
